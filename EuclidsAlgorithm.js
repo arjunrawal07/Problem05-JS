5d650d03aeb324f8bdbd2dded229c8129b070d55
ARJUN_RAWAL <arjunrawal07@gmail.com>
2020-10-02 16:39:43 -0400

    recursive solution for euclids algo

--- EuclidsAlgorithm.js
+++ EuclidsAlgorithm.js
@@ -1,9 +1,14 @@
 class EuclidsAlgorithm {
-    gcd(a, b){
-        let divisor = -1;
-
-        return divisor;
+  gcd(a, b) {
+    //base cases
+    if (a === 0) {
+      return b;
     }
+    if (b === 0) {
+      return a;
+    }
+    return this.gcd(b, a % b);
+  }
 }
 
 module.exports = EuclidsAlgorithm;
